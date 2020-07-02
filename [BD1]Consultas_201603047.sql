3c931fce7853e2be7dc131c17548b9ed140ce692
Jefferson Geovanny <jeffgeomp@gmail.com>
2020-07-01 23:28:26 -0600

    Contadores de Tablas

---
+++ [BD1]Consultas_201603047.sql
@@ -0,0 +1,112 @@
+USE Proyecto2;
+
+/*
+-------------------------- CONSULTA NO. 1 --------------------------------------
+Desplegar para cada elección el país y el partido político que obtuvo 
+mayor porcentaje de votos en su país. Debe desplegar el nombre de la elección, 
+el año de la elección, el país, el nombre del partido político y el porcentaje
+que obtuvo de votos en su país.
+*/
+
+/*
+-------------------------- CONSULTA NO. 2 --------------------------------------
+Desplegar total de votos y porcentaje de votos de mujeres por departamento y país.
+El ciento por ciento es el total de votos de mujeres por país. (Tip: Todos los
+porcentajes por departamento de un país deben sumar el 100%)
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 3 --------------------------------------
+Desplegar el nombre del país, nombre del partido político y número de alcaldías de
+los partidos políticos que ganaron más alcaldías por país.
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 4 --------------------------------------
+Desplegar todas las regiones por país en las que predomina la raza indígena. 
+Es decir, hay más votos que las otras razas.
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 5 --------------------------------------
+Desplegar el nombre del país, el departamento, el municipio, el partido político y
+la cantidad de votos universitarios de todos aquellos partidos políticos que 
+obtuvieron una cantidad de votos de universitarios mayor que el 25% de votos 
+de primaria y menor que el 30% de votos de nivel medio 
+(correspondiente a ese municipio y al partido político).  
+Ordene sus resultados de mayor a menor.
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 6 --------------------------------------
+Desplegar el porcentaje de mujeres universitarias y hombres universitarios que 
+votaron por departamento, donde las mujeres universitarias que votaron fueron más 
+que los hombres universitarios que votaron.
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 7 --------------------------------------
+Desplegar el nombre del país, la región y el promedio de votos por departamento. 
+Por ejemplo: si la región tiene tres departamentos, se debe sumar todos los votos 
+de la región y dividirlo dentro de tres (número de departamentos de la región).
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 8 --------------------------------------
+Desplegar el nombre del municipio y el nombre de los dos partidos políticos con 
+más votos en el municipio, ordenados por país.
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 9 --------------------------------------
+Desplegar el total de votos de cada nivel de escolaridad (primario, medio, universitario) 
+por país, sin importar raza o sexo.
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 10 -------------------------------------
+Desplegar el nombre del país y el porcentaje de votos por raza.
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 11 -------------------------------------
+Desplegar el nombre del país en el cual las elecciones han sido más peleadas. 
+Para determinar esto se debe calcular la diferencia de porcentajes de votos entre 
+el partido que obtuvo más votos y el partido que obtuvo menos votos.
+*/
+
+
+/*
+-------------------------- CONSULTA NO. 12 -------------------------------------
+Desplegar el total de votos y el porcentaje de votos emitidos por mujeres 
+indígenas alfabetas.
+*/
+
+/*
+-------------------------- CONSULTA NO. 13 -------------------------------------
+Desplegar el nombre del país, el porcentaje de votos de ese país en el que han 
+votado mayor porcentaje de analfabetas. (tip: solo desplegar un nombre de país, 
+el de mayor porcentaje).
+*/
+
+/*
+-------------------------- CONSULTA NO. 14 -------------------------------------
+Desplegar la lista de departamentos de Guatemala y número de votos obtenidos, 
+para los departamentos que obtuvieron más votos que el departamento de Guatemala.
+*/
+
+/*
+-------------------------- CONSULTA NO. 15 -------------------------------------
+Desplegar el total de votos de los municipios agrupados por su letra inicial. 
+Es decir, agrupar todos los municipios con letra A y calcular su número de votos, 
+lo mismo para los de letra inicial B, y así sucesivamente hasta la Z.
+*/
