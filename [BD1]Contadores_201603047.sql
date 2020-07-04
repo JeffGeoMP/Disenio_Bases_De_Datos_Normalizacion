5ddf4ad67b21d4cd7f0f6156e7e0bb4e51f3b0c9
Jefferson Geovanny <jeffgeomp@gmail.com>
2020-07-03 21:42:57 -0600

    Update Contadores

--- [BD1]Contadores_201603047.sql
+++ [BD1]Contadores_201603047.sql
@@ -1,10 +1,9 @@
 USE Proyecto2;
 
-SELECT 'Votos' AS Contador, SUM(analfabetos + alfabetos) AS Total FROM detalle_eleccion UNION
+SELECT 'Votos' AS Contador, COUNT(*) AS Total FROM detalle_eleccion UNION
 SELECT 'Partidos' AS Contador, COUNT(*) AS Total FROM partido UNION
-SELECT 'Municipios' AS Contador, COUNT(*) AS Total FROM municipio UNION
+SELECT 'Municipios' AS Contador, COUNT(*) AS Total FROM (SELECT DISTINCT id_pais, id_departamento, id_municipio FROM zona) td1 UNION
 SELECT 'Departamentos' AS Contador, COUNT(*) AS Total FROM (SELECT DISTINCT id_pais, id_departamento FROM zona) td1 UNION
 SELECT 'Regiones' AS Contador, COUNT(*) AS Total FROM (SELECT DISTINCT Region FROM departamento) td1 UNION
 SELECT 'Paises' AS Contador, COUNT(*) AS Total FROM pais UNION
-SELECT 'Elecciones' AS Contador, COUNT(*) AS Total FROM eleccion UNION
-SELECT 'Elecciones2' AS Contador, COUNT(*) AS Total FROM (SELECT DISTINCT nombre, año FROM eleccion) td1;
+SELECT 'Elecciones' AS Contador, COUNT(*) AS Total FROM (SELECT DISTINCT nombre, año FROM eleccion) td1;
