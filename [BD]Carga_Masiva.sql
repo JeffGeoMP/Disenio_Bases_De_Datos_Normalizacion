f95f66bcd5905f871d0abc6dc454060a4d1f0faa
Jefferson Geovanny <jeffgeomp@gmail.com>
2020-07-11 23:18:38 -0600

    Carga Masiva

--- [BD]Carga_Masiva.sql
+++ [BD]Carga_Masiva.sql
@@ -31,7 +31,7 @@ UNIVERSITARIOS		INT
 /*----------------------------------------------------------------------------------------*/
 
 /*-------------------- Carga Masiva de Datos a partir del CSV ----------------------------*/
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ICE-FUENTE-MEJORADO.csv'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ICE-FUENTE.csv'
 INTO TABLE Proyecto2.data_temporal
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
